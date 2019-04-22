d155b731a479c4cff5a2cab42a2dc6db75ac141c
nilim <hello@MacBooks-MacBook-Pro.local>
2019-04-22 22:07:50 +0600

    sys info done init

--- sys.py
+++ sys.py
@@ -2,28 +2,46 @@ import psutil
 import cpuinfo
 import pprint
 
+
 def getCpuInfo():
     cpu_obj: dict = {}
     cpu_info = cpuinfo.get_cpu_info().items()
-
     for key, value in cpu_info:
         cpu_obj.update({key : value})
-    
     del cpu_obj['flags']
     return cpu_obj
+
 
 def getMemoryInfo():
     memory_obj: dict = {}
     memory = psutil.virtual_memory()
-    # for key, value in memory:
-    #     memory_obj.update({key: value})
-    print(type(memory))
+    for key in memory._fields:
+        value = getattr(memory, key)
+        memory_obj.update({key: value})
+    return memory_obj
 
-# def getDiskInfo():
 
-# def getProcessInfo():
+def getDiskInfo():
+    disk_obj: dict = {}
+    disk = psutil.disk_usage('/')
+    for key in disk._fields:
+        value = getattr(disk, key)
+        disk_obj.update({key: value})
+    return disk_obj
 
-# def getNetworkInfo():
+def getProcessInfo():
+    # for x in psutil.process_iter():
+    #     print(x)
+    process = [p.info for p in psutil.process_iter(attrs=['name', 'username'])]
+    return process
+
+def getNetworkInfo():
+    net_obj = {}
+    net = psutil.net_io_counters()
+    for key in net._fields:
+        value = getattr(net, key)
+        net_obj.update({key: value})
+    return net_obj
 
 def main():
     all_info: dict = {}
