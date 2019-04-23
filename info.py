2e5e97df17069c32a1876917d373fa5655d3c637
snanilim <snanilim@gmail.com>
2019-04-23 18:22:44 +0600

    mqtt init

--- info.py
+++ info.py
@@ -1,5 +1,6 @@
 # https://thispointer.com/python-get-list-of-all-running-processes-and-sort-by-highest-memory-usage/
 # https://www.pythoncircle.com/post/535/python-script-9-getting-system-information-in-linux-using-python-script/
+# http://www.steves-internet-guide.com/into-mqtt-python-client/
 # Python script to fetch system information
 # Author -  ThePythonDjango.Com
 # Tested with Python3 on Ubuntu 16.04
