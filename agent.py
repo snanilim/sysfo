e3d62b5ad8b5d63eba419d752d77c7e326948bc4
snanilim <snanilim@gmail.com>
2019-05-07 15:47:54 +0600

    set new topic and msg

--- agent.py
+++ agent.py
@@ -2,9 +2,8 @@ from agent_pc_info import *
 import time
 import paho.mqtt.client as mqtt
 import random
-import re, uuid 
-
-
+import re, uuid
+import json
 
 
 
@@ -13,6 +12,7 @@ r = str(uuid.uuid4())
 client = mqtt.Client(r)
 broker = "broker.hivemq.com"
 port = 1883
+
 mac_addr = hex(uuid.getnode()).replace('0x', '')
 ':'.join(mac_addr[i : i + 2] for i in range(0, 11, 2))
 keep_alive = {
@@ -21,58 +21,83 @@ keep_alive = {
 }
 keep_alive = str(keep_alive)
 
+division = None
+district = None
+upazilla = None
+lab_id = None
+
+
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code " + str(rc))
 
-def on_publish(client, userdata, mid):
-	# print('message')
-	client.publish("web/hello", keep_alive)
-	# time.sleep(10)
 
 def on_message(client, userdata, message):
-	# print(message.payload)
-    print("topic: "+message.topic+"	"+"payload: "+str(message.payload.decode("utf-8")))
-    send_data_to_broker(str(message.payload.decode("utf-8")))
-
-
-   
-  
-
+	print("topic: "+message.topic+"	"+"payload: "+str(message.payload.decode("utf-8")))
+	check_topic(str(message.payload.decode("utf-8")))
+	send_data_to_broker(str(message.payload.decode("utf-8")))
 
 client.on_connect = on_connect  #attach the callback function to the client object 
 client.on_message = on_message	#attach the callback function to the client object
-client.on_publish = on_publish
+
 
 
 client.connect(broker,port,60)
 print ("connecting to broker")
 
 
-client.subscribe("srdl/req_info/test")
-client.subscribe("srdl/each_info/{mac_addr}")
-# client.subscribe("web/hello", 0)
-# client.publish("web/hello", keep_alive)
-print ("subscribed")
+# client.subscribe("srdl/req_info/test")
+client.subscribe(f"srdl/req_info/{division}/{district}/{upazilla}/{lab_id}/{mac_addr}/")
+client.subscribe(f"srdl/res_topic/{mac_addr}", 1)
+if district is None:
+	client.publish(f"srdl/req_topic/{mac_addr}", str({"topic": 1}))
+print ("subscribed", f"srdl/req_topic/{mac_addr}")
+
+
+def check_topic(msg):
+	global division, district, upazilla, lab_id
+	if (division is None and district is None and upazilla is None and lab_id is None):
+		data = json.loads(msg)
+		topic_value = data.get("topic", "")
+
+		if 'topic' in data and topic_value == 1:
+			if 'division' in data:
+				value = data.get("division", "")
+				division = value
+				print('msges', value)
+
+			if 'district' in data:
+				value = data.get("district", "")
+				district = value
+				print('msges', value)
+
+			if 'upazilla' in data:
+				value = data.get("upazilla", "")
+				upazilla = value
+				print('msges', value)
+
+			if 'lab_id' in data:
+				value = data.get("lab_id", "")
+				lab_id = value
+				print('msges', value)
+		else:
+			client.publish(f"srdl/req_topic/{mac_addr}", str({"topic": 1}))
+		# print(f"srdl/req_topic/{mac_addr}")
+		# district = "Dhaka"
+	else:
+		return True
 
 def send_data_to_broker(msg):
-	msg_list = msg.split(',')
-	print('msg', msg_list[0])
+	data = json.loads(msg)
+	info_value = data.get("info", "")
 
-	if(msg_list[0] == '1'):
-		
+	if 'info' in data and info_value == 1:
 		print("I'm here")
-		res_info = info(msg_list)
+		res_info = info(data)
 		# print(res_info)
-		client.publish("srdl/info/test", str(res_info))
+		client.publish(f"srdl/res_info/{division}/{district}/{upazilla}/{lab_id}/{mac_addr}", str(res_info))
 
 	
-	if(msg_list[0] == '1'):
-		from idle import get_idle_time
-		print("Im not here")
-		res_info = get_idle_time()
-		print(res_info)
-		client.publish("srdl/idle/test", str(res_info))
 	
 
 client.loop_forever() # to maintain continuous network traffic flow with the broker
