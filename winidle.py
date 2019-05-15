622e99c65d2e69bd630e7cbb45fe3284089e5805
snanilim <snanilim@gmail.com>
2019-05-15 14:49:04 +0600

    windows get idle time

---
+++ winidle.py
@@ -0,0 +1,16 @@
+from ctypes import Structure, windll, c_uint, sizeof, byref
+
+class LASTINPUTINFO(Structure):
+    _fields_ = [
+        ('cbSize', c_uint),
+        ('dwTime', c_uint),
+    ]
+
+def get_idle_duration():
+    lastInputInfo = LASTINPUTINFO()
+    lastInputInfo.cbSize = sizeof(lastInputInfo)
+    windll.user32.GetLastInputInfo(byref(lastInputInfo))
+    millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+    return millis / 1000.0
+
+print(get_idle_duration())
